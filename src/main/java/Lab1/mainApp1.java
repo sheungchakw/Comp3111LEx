73b092988b0b3f214a7b15cd119cda318af54244
sheungchakw <billwongacctesting@gmail.com>
2026-09-20 00:55:38 +0800

    Add 2 print lines as report heading.

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -9,6 +9,7 @@ public class mainApp1 {
         int n = 11;
         System.out.println("Comp3111 Lab1 Report Heading");
         System.out.println("Student Name:  WONG, Sheung Chak, Student ID: 21178604");
+        System.out.println("Program Starting...");
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
         System.out.println(n + "! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
